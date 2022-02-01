df2051f7796d66befc79363d69c0aa19bf366c32
Nicholas Martini <nicho.martini@gmail.com>
2022-02-01 16:48:38 +0100

    Syntax adjustments.

--- ConsoleMenuManager/Page.cs
+++ ConsoleMenuManager/Page.cs
@@ -16,19 +16,19 @@
         public void Display()
         {
             Console.WriteLine(prompt);
-            Console.WriteLine();
             Console.WriteLine("-----");
+            Console.WriteLine();
             for (int i = 0; i < options.Count; i++)
             {
                 Console.WriteLine($"{i + 1}- {options[i].name}");
             }
-
-            Console.WriteLine("Choose an option");
+            Console.WriteLine();
+            Console.WriteLine("Choose an option:");
             var input = Console.ReadLine();
             int menuChoice;
             while (!int.TryParse(input, out menuChoice) || menuChoice < 1 || menuChoice > options.Count)
             {
-                Console.WriteLine($"Please enter an integer number between 1 and {options.Count}");
+                Console.WriteLine($"Please enter an integer number between 1 and {options.Count}:");
                 input = Console.ReadLine();
             }
 
