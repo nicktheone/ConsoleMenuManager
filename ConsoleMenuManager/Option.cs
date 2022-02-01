910ce0971bf804ac628bb6b95e72488033ec7086
Nicholas Martini <nicho.martini@gmail.com>
2022-02-01 15:29:21 +0100

    Added properties and constructor.

--- ConsoleMenuManager/Option.cs
+++ ConsoleMenuManager/Option.cs
@@ -2,5 +2,12 @@
 {
     public class Option
     {
+        internal readonly string name;
+        internal readonly Action callback;
+        public Option(string name, Action callback)
+        {
+            this.name = name;
+            this.callback = callback;
+        }
     }
 }
