b145079a1e007a6e96fcf7009a32bc3cd792b17a
Nicholas Martini <nicho.martini@gmail.com>
2022-02-01 16:01:41 +0100

    Made class abstract and added method for displaying menu items.

--- ConsoleMenuManager/Page.cs
+++ ConsoleMenuManager/Page.cs
@@ -1,19 +1,36 @@
 ﻿namespace ConsoleMenuManager
 {
-    public class Page
+    public abstract class Page
     {
-        private string title;
-        private List<Option> options;
+        private readonly string prompt;
+        private readonly List<Option> options;
 
-        public Page(string title, List<Option> options )
+        public Page(string prompt, List<Option> options)
         {
-            this.title = title;
+            this.prompt = prompt;
             this.options = options;
         }
 
         public void Display()
         {
+            Console.WriteLine(prompt);
+            Console.WriteLine();
+            Console.WriteLine("-----");
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}- {options[i].name}");
+            }
 
+            Console.WriteLine("Choose an option");
+            var input = Console.ReadLine();
+            int menuChoice;
+            while (!int.TryParse(input, out menuChoice) || menuChoice < 1 || menuChoice > options.Count)
+            {
+                Console.WriteLine($"Please enter an integer number between 1 and {options.Count}");
+                input = Console.ReadLine();
+            }
+
+            options[menuChoice - 1].callback.Invoke();
         }
     }
 }
